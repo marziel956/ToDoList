52096874452ee205d981cd446104909a2311ca50
marziel956 <marziel956@gmail.com>
2024-08-19 13:22:11 +0200

    add tableviewcontroller and some items

---
+++ ToDoList/ToDoListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Marcin Zieliński on 19/08/2024.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggs", "Go to Gym", "Clean House", "Pay Bills"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+
+    }
+}
+
