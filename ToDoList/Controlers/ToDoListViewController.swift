d3dda7f0f58ecbcb50f8b741af867b9f4bcb5423
marziel956 <marziel956@gmail.com>
2024-08-28 00:40:31 +0200

    delete the unnecessary code and comments

--- ToDoList/Controlers/ToDoListViewController.swift
+++ ToDoList/Controlers/ToDoListViewController.swift
@@ -22,55 +22,21 @@ class ToDoListViewController: UITableViewController {
         
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
 
-       loadItem()
-        
-//        let newItem1 = Item()
-//        newItem1.title = "New Item1"
-//        itemArray.append(newItem1)
-//        
-//        let newItem2 = Item()
-//        newItem2.title = "New Item2"
-//        itemArray.append(newItem2)
-//        
-//        let newItem3 = Item()
-//        newItem3.title = "New Item3"
-//        itemArray.append(newItem3)
-        
-      //  if let items = defaults.array(forKey: itemKey) as? [Item]{
-            
-          //  itemArray = items
+        loadItem()
+        } // Do any additional setup after loading the view.
 
-        }
-         
-        // Do any additional setup after loading the view.
-    
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
-    
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
 
-        
         let item = itemArray[indexPath.row] // uproszenie i skrócenie kodu dodając nowa stałą
         
         cell.textLabel?.text = item.title
         
-        cell.accessoryType =  item.isDone ? .checkmark : .none   // aktywowanie checkmark'a kiedy nie jest włączony i jego dezaktywacja kod ponizej wykonuje to samo
-       
-         /*
-          
-        if item.isDone == true {
-            cell.accessoryType = .checkmark
-        }else {
-            cell.accessoryType = .none
-        }]
-          
-        */
-        
+        cell.accessoryType =  item.isDone ? .checkmark : .none   // aktywowanie checkmark'a kiedy nie jest włączony i jego dezaktywacja
         
         return cell
     }
@@ -84,33 +50,20 @@ class ToDoListViewController: UITableViewController {
     // - return: Zwraca konfigurowany obiekt `UITableViewCell`.
     
     
-    
-    
-    
     //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row]) - wypis w konsoli który wiersz jest wybierany
+    
         
-        itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
-        // kod wyzej wykonuje dokładnie to samo co poniżej
-/*
-        if itemArray[indexPath.row].isDone == false {
-            itemArray[indexPath.row].isDone = true
-        }else{
-                itemArray[indexPath.row].isDone = false
-        }
-*/
+        context.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
+        
+       // itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
+
         saveItems()
         
-        
-    
         tableView.deselectRow(at: indexPath, animated: true)  // wyłączenie i właczenie podswietlenia kiedy dotykamy na dany wiersz
     }
-    
-    
-    
-    
     
     //MARK: - Add New Items
     
@@ -120,11 +73,9 @@ class ToDoListViewController: UITableViewController {
         
         let alert = UIAlertController(title: "Dodaj zadanie", message: "", preferredStyle: .alert)
         
-        
         let action = UIAlertAction(title: "Dodaj", style: .default) { (action) in
             //co sie stanie gdy użytkownik kliknie przyciks dodaj(plus)
-            
-                           
+    
             let newItem = Item(context: self.context)
             newItem.title = alertText.text!
             newItem.isDone = false
@@ -132,50 +83,30 @@ class ToDoListViewController: UITableViewController {
             self.itemArray.append(newItem) // dodanie elementu do tablicy 'itemArray'
             
             self.saveItems()
-       
-        
-           
         }
-        
-        
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Dodaj nowe zaplanowane zadanie"
             alertText = alertTextField
-            
         }
        
-    
         alert.addAction(action)
         
     present(alert, animated: true, completion: nil)
     
-        
     }
     
     //MARK: - Model Manupulation Methods
-
-
-
+    
     func saveItems() {
-
-        
-        
         do{
-           
            try context.save()
-          
         }catch {
-            
         print("error saving context, \(error)")
-            
         }
-       
         self.tableView.reloadData() // ponowne załadowanie danych w tabeli
     }
-    
-    
-    
+
     func loadItem() {
         let request: NSFetchRequest<Item> = Item.fetchRequest()
         do{
