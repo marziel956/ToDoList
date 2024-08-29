754680b5813ca87b2ece96df5f9466e6cc254bb3
marziel956 <marziel956@gmail.com>
2024-08-29 23:33:09 +0200

    Add new functions and improving errors

--- ToDoList/Controlers/ToDoListViewController.swift
+++ ToDoList/Controlers/ToDoListViewController.swift
@@ -9,29 +9,48 @@
 import UIKit
 import CoreData
 
-class ToDoListViewController: UITableViewController {
-
+class ToDoListViewController: UITableViewController{
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var itemArray = [Item]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     // zawarty tu kod daje dostęp do AppDelegate)
     
     //let itemKey = "TodoListArray"
-  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    
+        
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+//           tapGesture.cancelsTouchesInView = false
+//           view.addGestureRecognizer(tapGesture)
+        
+        
+        searchBar.delegate = self
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-
-        loadItem()
-        } // Do any additional setup after loading the view.
-
+        
+                
+        loadItems()
+    } // Do any additional setup after loading the view.
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }// metoda która pozwala ukryc klawiature
+    
+    override func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+            hideKeyboard()
+        }// ukrywanie klawiatury kiedy przewijamy tabele
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-
+        
         let item = itemArray[indexPath.row] // uproszenie i skrócenie kodu dodając nowa stałą
         
         cell.textLabel?.text = item.title
@@ -53,13 +72,14 @@ class ToDoListViewController: UITableViewController {
     //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    
         
-        context.delete(itemArray[indexPath.row])
-        itemArray.remove(at: indexPath.row)
         
-       // itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
-
+        //context.delete(itemArray[indexPath.row])
+        //itemArray.remove(at: indexPath.row)
+       
+        
+        itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
+        
         saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)  // wyłączenie i właczenie podswietlenia kiedy dotykamy na dany wiersz
@@ -75,11 +95,11 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Dodaj", style: .default) { (action) in
             //co sie stanie gdy użytkownik kliknie przyciks dodaj(plus)
-    
+            
             let newItem = Item(context: self.context)
             newItem.title = alertText.text!
             newItem.isDone = false
-          
+            
             self.itemArray.append(newItem) // dodanie elementu do tablicy 'itemArray'
             
             self.saveItems()
@@ -89,35 +109,81 @@ class ToDoListViewController: UITableViewController {
             alertTextField.placeholder = "Dodaj nowe zaplanowane zadanie"
             alertText = alertTextField
         }
-       
+        
         alert.addAction(action)
         
-    present(alert, animated: true, completion: nil)
-    
+        present(alert, animated: true, completion: nil)
+        
     }
     
     //MARK: - Model Manupulation Methods
     
     func saveItems() {
         do{
-           try context.save()
+            try context.save()
         }catch {
-        print("error saving context, \(error)")
+            print("error saving context, \(error)")
         }
         self.tableView.reloadData() // ponowne załadowanie danych w tabeli
     }
-
-    func loadItem() {
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
+    
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+//let request: NSFetchRequest<Item> = Item.fetchRequest()
         do{
             itemArray = try context.fetch(request)
         }catch{
             print("error fetching data from context, \(error)")
         }
+        self.tableView.reloadData()
+    }
+    
+}
+//MARK: - Search bar methods
+//extension ToDoListViewController: UISearchBarDelegate {
+//    
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        let request: NSFetchRequest<Item> = Item.fetchRequest()
+//        
+//        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+//        
+//        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+//    
+//        loadItems(with: request)
+//      
+//        
+//    }
+//    
+//    
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        if searchBar.text?.count == 0 {
+//            loadItems()
+//           
+//        }
+//    }
+//}
+extension ToDoListViewController: UISearchBarDelegate {
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            // Załaduj wszystkie elementy, jeśli pole wyszukiwania jest puste
+            loadItems()
+            
+        } else {
+            // Przefiltruj wyniki na podstawie wpisanego tekstu
+            let request: NSFetchRequest<Item> = Item.fetchRequest()
+            request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchText)
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            
+            loadItems(with: request)
+            
+           
+        }
+        
+        
+        // Odśwież tabelę po każdej zmianie tekstu
+        tableView.reloadData()
     }
 }
-
-
 
 
 
