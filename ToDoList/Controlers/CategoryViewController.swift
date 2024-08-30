f282597c0ea3a288ddc3fdcfe5f855eebbf55068
marziel956 <marziel956@gmail.com>
2024-08-30 09:37:28 +0200

    add category

---
+++ ToDoList/Controlers/CategoryViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryViewController.swift
+//  ToDoList
+//
+//  Created by Marcin Zieliński on 30/08/2024.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    var categoryArray = [Category]()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    // MARK: - Add New Categoris
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+    // MARK: - TableView Datasource Methods
+    
+    
+    // MARK: - TableView Delegate Methods
+
+    // MARK: - Data Manipulation Methods
+    // tutaj trzeba napisac co zrobic aby po wybraniu danej celi przejsc na nowy widok
+    
+}
